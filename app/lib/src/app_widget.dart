6133b940638b9f6476dac94e6c700add027d6de6
matheus <mattmvc56@gmail.com>
2023-02-11 21:20:57 -0400

    initial comiit

---
+++ app/lib/src/app_widget.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
+
+import 'shared/themes/themes.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Modular.setInitialRoute('/home/');
+    return MaterialApp.router(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: lightTheme,
+      darkTheme: darkTheme,
+      routerDelegate: Modular.routerDelegate,
+      routeInformationParser: Modular.routeInformationParser,
+    );
+  }
+}
---
+++ app/lib/src/shared/widgets/user_image_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class UserImageButton extends StatelessWidget {
+  const UserImageButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final colorScheme = theme.colorScheme;
+    return GestureDetector(
+      onTap: () {},
+      child: Padding(
+        padding: const EdgeInsets.all(8),
+        child: CircleAvatar(
+          backgroundColor: colorScheme.primary,
+          foregroundColor: colorScheme.onPrimary,
+          child: const Text('M'),
+        ),
+      ),
+    );
+  }
+}
